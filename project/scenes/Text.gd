b440f37081d69fec1eac1e9b6fee53241eb0439e
Gilan <58899528+GilanRanger@users.noreply.github.com>
2023-03-09 22:58:16 -0800

    Delete project directory

--- project/scenes/Text.gd
+++
@@ -1,9 +0,0 @@
-extends CanvasLayer
-
-signal start_game
-
-func _input(event):
-	if event is InputEventKey and event.scancode == KEY_SPACE:
-		self.remove_child(get_child(0))
-		self.remove_child(get_child(1))
-		emit_signal("start_game")
