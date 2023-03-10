971a1002a240b670d5c0bc3f0a17c2f0c9e96efd
Gilan <58899528+GilanRanger@users.noreply.github.com>
2023-03-09 22:25:36 -0800

    Delete scenes directory

--- scenes/Oil.gd
+++
@@ -1,8 +0,0 @@
-extends KinematicBody2D
-
-signal cleaned
-
-func _physics_process(delta):
-	var collision = move_and_collide(Vector2.ZERO);
-	if collision:
-		get_parent().remove_child(self)
--- scenes/Sprite.gd
+++
@@ -1,7 +0,0 @@
-extends Sprite
-
-func _ready():
-	self.set_position(Vector2(69.634, 5.784))
-
-func _on_OilSpawner_game_over():
-	self.set_position(Vector2(510, 295))
